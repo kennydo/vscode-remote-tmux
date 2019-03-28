8f7a615952507f5839444e0e66630c2a1b79b158
Kenny Do <chinesedewey@gmail.com>
2019-03-28 16:49:53 -0700

    Add configuration entries for SSH host and tmux session name

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,14 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+	let config = vscode.workspace.getConfiguration('remoteTmux');
+
+	let sshHost = config.get("sshHost");
+	let tmuxSessionName = config.get("tmuxSessionName");
+
+	console.log("Configured SSH host: " + sshHost);
+	console.log("Configured tmux session name: " + tmuxSessionName);
+
 	let disposable = vscode.commands.registerCommand('remoteTmux.getOrCreateTerminal', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
