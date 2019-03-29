fd701965b6b717291f2686353a4b6f54b0fc97f1
Kenny Do <chinesedewey@gmail.com>
2019-03-29 11:30:52 -0700

    Handle having only 1 terminal for remote tmux

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,19 @@ import * as vscode from 'vscode';
 import {isSafeSshHost, isSafeTmuxSessionName} from './stringUtils';
 
 export function activate(context: vscode.ExtensionContext) {
-	let config = vscode.workspace.getConfiguration('remoteTmux');
+	const config = vscode.workspace.getConfiguration('remoteTmux');
 
-	let sshHost = config.get("sshHost", "devbox");
-	let tmuxSessionName = config.get("tmuxSessionName", "vscode");
-
-	console.log("Configured SSH host: " + sshHost);
-	console.log("Configured tmux session name: " + tmuxSessionName);
+	// We keep track of the tmux terminal that we've created so that we can bring it up whenever the command is run.
+	// There isn't a point to having multiple terminals open to the same tmux session.
+	let existingTerminal: vscode.Terminal | null = null;
 
 	let disposable = vscode.commands.registerCommand('remoteTmux.getOrCreateTerminal', () => {
+		const sshHost = config.get("sshHost", "devbox");
+		const tmuxSessionName = config.get("tmuxSessionName", "vscode");
+
+		console.log("Configured SSH host: " + sshHost);
+		console.log("Configured tmux session name: " + tmuxSessionName);
+
 		if (!isSafeSshHost(sshHost)){
 			vscode.window.showErrorMessage(`SSH host '${sshHost}' looked dangerous or invalid, so aborting remote tmux invocation`);
 			return;
@@ -20,9 +24,33 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showErrorMessage(`Tmux session name '${tmuxSessionName}' looked dangerous or invalid, so aborting remote tmux invocation`);
 			return;
 		}
+
+		if (existingTerminal !== null) {
+			console.log("Focusing on existing terminal");
+			existingTerminal.show(false);
+		} else {
+			const terminal = vscode.window.createTerminal({
+				'name': `tmux on ${sshHost}`,
+				'shellPath': '/bin/bash',
+				'shellArgs': [
+					'-c',
+					`ssh -t ${sshHost} "tmux new-session -A -s ${tmuxSessionName}"`,
+				],
+			});
+			terminal.show(false);
+			existingTerminal = terminal;
+		}
 	});
 
 	context.subscriptions.push(disposable);
+
+	vscode.window.onDidCloseTerminal((terminal) => {
+		if (terminal === existingTerminal){
+			console.log(`Detected closing of the tmux terminal: ${existingTerminal.name}`);
+
+			existingTerminal = null;
+		}
+	});
 }
 
 export function deactivate() {}
